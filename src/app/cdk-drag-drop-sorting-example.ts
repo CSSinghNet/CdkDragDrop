7af2d2f8f397a1b1f99a151f8b9c678456e4c648
CSSinghNet <chandrashekharsingh25@gmail.com>
2023-11-15 17:45:29 +0530

    Add delete ,Up, Down button for add content in job

--- src/app/cdk-drag-drop-sorting-example.ts
+++ src/app/cdk-drag-drop-sorting-example.ts
@@ -17,8 +17,8 @@ import { MatDialog } from '@angular/material/dialog';
 export class CdkDragDropSortingExample {
   selectedJobIndex: number;
   jobList: any[];
-  idxList: any[];
-  ChildidxList: any[];
+  idxList: number[]=[];
+  ChildidxList: number[]=[];
   positions = [
     {
       name: 'Position 1',
@@ -128,28 +128,33 @@ export class CdkDragDropSortingExample {
   }
 
   onDownClick(jobIndex: number) {
-    this.processSelectedPositions(jobIndex);
+    for(const idx of this.ChildidxList){
+      transferArrayItem(this.jobList[jobIndex].children,this.positions, idx, idx);
+    }
+    this.positions = this.updateObjectInArray(this.positions,'isSelect', false); 
   }
 
   private processSelectedPositions(jobIndex: number) {
-    const isSelectPosition = this.positions.filter((x) => x.isSelect === true);
-    for (let idx of this.idxList) {
+    const selectedPositions = this.positions.filter((position) => position.isSelect);
+    for (const idx of this.idxList) {
       transferArrayItem(
-        isSelectPosition,
+        selectedPositions,
         this.jobList[jobIndex].children,
         idx,
         idx
-      );
+        );
+      this.jobList = this.updateObjectInArray(this.jobList,'isSelect', false);       
     }
-    this.positions = this.positions.filter((item) => item.isSelect !== true);
+    this.positions = this.positions.filter((position) => !position.isSelect);
   }
+  
 
   onJobSelected(): void {
     // Get the index of the selected position
     this.idxList = this.findSelectedIndex(this.positions);
   }
-  onJobChildSelected(item:any[]){
-this.ChildidxList=this.findSelectedIndex(item);
+  onJobChildSelected(array:any){
+   this.ChildidxList=this.findSelectedIndex(array);
   }
 
   findSelectedIndex(array: any[]) {
@@ -161,4 +166,40 @@ this.ChildidxList=this.findSelectedIndex(item);
     });
     return this.idxList;
   }
+
+  findJobSelectedIndex(array: any[]) {
+    // Reset idxList before processing the array
+    this.ChildidxList = [];
+    array.forEach((position, index) => {
+      if (position.isSelect) {
+        this.ChildidxList.push(index);
+      }
+    });
+  
+    return this.ChildidxList;
+  }
+
+  delete(indextoDelete:number){
+   this.jobList= this.jobList.filter((_, index) => index !== indextoDelete);
+  }
+
+
+ updateObjectInArray(array:any, keyToUpdate:string, newValue:boolean) {
+  return array.map((obj:any) => {
+      // Check if the object has a 'children' property
+      if (obj.children && Array.isArray(obj.children)) {
+          // If yes, update the nested object
+          obj.children = obj.children.map((nestedObj:any) => {
+              // If yes, create a new nested object with the updated key-value pair
+              return { ...nestedObj, [keyToUpdate]: newValue };
+          });
+      } else {
+          // If no 'children' property, update the object directly
+          return { ...obj, [keyToUpdate]: newValue };
+      }
+      // Return the original object (with or without changes)
+      return obj;
+  });
+}
+
 }
